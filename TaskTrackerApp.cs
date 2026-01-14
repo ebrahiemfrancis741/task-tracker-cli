10e0525debc1c3e43b3619d81aeb38af78265c1f
ebrahiemfrancis741 <ebrahiemfrancis741@gmail.com>
2026-01-14 20:06:33 +0200

    Add cliAddTask(string[] args

--- TaskTrackerApp.cs
+++ TaskTrackerApp.cs
@@ -6,16 +6,45 @@ namespace task_tracker_cli
 {
     public class TaskTrackerApp
     {
+        static TaskTracker taskTracker;
         static void Main(string[] args)
         {
-            TaskTracker taskTracker = new TaskTracker();
-            //taskTracker.loadTasksFromFile("tasks.json");
-            //Task task = new Task(1, "learn math", "todo", DateTime.Now, DateTime.Now);
-            //taskTracker.addTask(task);
-            //taskTracker.addTask(new Task(2,"learn c#", "in-progress", DateTime.Now, DateTime.Now));
-            taskTracker.displayAllTasks();
-            //taskTracker.saveTasksToFile("tasks.json");
+            taskTracker = new TaskTracker();
 
+            if (args.Length <= 0)
+            {
+                Console.WriteLine("Use arguments to run the app");
+                return;
+            }
+
+            switch (args[0])
+            {
+                case "add":
+                    cliAddTask(args);
+                    break;
+                default:
+                    Console.WriteLine("undefined operation specified");
+                    break;
+            }
+            taskTracker.saveTasksToFile("tasks.json");
+        }
+
+        static void cliAddTask(string[] args)
+        {
+            if (args.Length != 3)
+            {
+                Console.WriteLine("Correct usage: add 'id' 'description'");
+                return;
+            }
+            Task task = new Task(Convert.ToInt32(args[1]), args[2], "todo", DateTime.Now, DateTime.Now);
+            if (taskTracker.addTask(task))
+            {
+                Console.WriteLine("Task added successfully (ID: {0})", args[1]);
+            }
+            else
+            {
+                Console.WriteLine("Failed to add task");
+            }
         }
     }
 }
